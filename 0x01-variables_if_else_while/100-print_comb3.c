e210d0af9399d243aeff71f4087b10dc665fac54
Amineafilal <amineafilalelalami@gmail.com>
2023-10-09 13:30:36 -0700

    10. Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Description : Write a program that prints the lowercase.
+ * Return: 0
+*/
+int main(void)
+{
+	int i, d;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (d = 48; d <= 57; d++)
+		{
+			if (i != d && i < d)
+			{
+				putchar(i);
+				putchar(d);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
