be9fa359959dc6e7b84618a7c6acf8aba5950eec
Amineafilal <amineafilalelalami@gmail.com>
2023-10-09 12:40:48 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Description : Write a program that prints the lowercase.
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+	putchar(n);
+	if (n != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
