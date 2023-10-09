024ef57e12facaf9259efb841f49a2e77fc2d776
Amineafilal <amineafilalelalami@gmail.com>
2023-10-09 11:25:53 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Description : Write a program that prints.
+ * Return: 0
+*/
+int main(void)
+{
+	int s;
+
+	for (s = 48; s <= 57; s++)
+	{
+	putchar(s);
+	}
+	putchar('\n');
+	return (0);
+}
