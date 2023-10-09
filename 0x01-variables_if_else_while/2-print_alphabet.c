731c973675e63bce4b6e609df2408fc0c9249dc0
Amineafilal <amineafilalelalami@gmail.com>
2023-10-09 09:48:00 -0700

    2. I sometimes suffer from insomnia.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Description : Write a program that prints the alphabet
+ * Return: 0
+*/
+int main(void)
+{
+	char n;
+
+	for (n = 97 ; n <= 122 ; n++)
+	{
+	putchar(n);
+	}
+	putchar('/n');
+	return (0);
+}
