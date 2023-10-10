87e9a343a64f33f836c09518530b98b6702968ed
Amineafilal <amineafilalelalami@gmail.com>
2023-10-10 12:54:47 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- entry point
+ * Description : Write a function that prints 10 times the alphabet.
+ * Return: 0
+*/
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
