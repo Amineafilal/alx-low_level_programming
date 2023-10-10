15ee917640ed61905f2cc8d8b10463f7b668ed57
Amineafilal <amineafilalelalami@gmail.com>
2023-10-10 11:34:46 -0700

    1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet- entry point
+ * Description : Write a function that prints the alphabet.
+ * Return: None (void)
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+	  _putchar(i);
+	}
+	_putchar('\n');
+}
