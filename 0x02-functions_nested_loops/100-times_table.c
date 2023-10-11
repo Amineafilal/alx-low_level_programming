7994d0ecfadd8494835e4514678a44311fa67e16
Amineafilal <amineafilalelalami@gmail.com>
2023-10-11 14:15:19 -0700

    The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,38 +9,40 @@ void print_times_table(int n)
 	int nums, mul, prodcalcul;
 
 	for (nums = 0; nums <= n; nums++)
-		if (n < 15 && n > 0)
+	{
+		if (n <= 15 && n >= 0)
 		{
-			_putchar(48);
+			putchar(48);
 			for (mul = 1; mul <= n; mul++)
 			{
 				prodcalcul = nums * mul;
-				_putchar(',');
+				putchar(',');
 
 				if (prodcalcul <= 9)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(prodcalcul + 48);
+					putchar(' ');
+					putchar(' ');
+					putchar(' ');
+					putchar(prodcalcul + 48);
 				}
 				else if (prodcalcul <= 99)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((prodcalcul / 10) + 48);
-					_putchar((prodcalcul % 10) + 48);
+					putchar(' ');
+					putchar(' ');
+					putchar((prodcalcul / 10) + 48);
+					putchar((prodcalcul % 10) + 48);
 				}
 
 				else
 				{
-					_putchar(' ');
+					putchar(' ');
 
-					_putchar((prodcalcul / 100) + 48);
-					_putchar(((prodcalcul - 100) / 10) + 48);
-					_putchar((prodcalcul % 10) + 48);
+					putchar((prodcalcul / 100) + 48);
+					putchar(((prodcalcul - 100) / 10) + 48);
+					putchar((prodcalcul % 10) + 48);
 				}
 			}
-			_putchar('\n');
+			putchar('\n');
 		}
+	}
 }
