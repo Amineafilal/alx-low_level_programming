22d703a2981e2637c17c38377a1af8096c2cc916
Amineafilal <amineafilalelalami@gmail.com>
2023-10-11 13:40:51 -0700

    The World looks like a multiplication-table.

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_times_table- prints the 9 times table, starting with 0
+ *
+ * @n: input
+ */
+void print_times_table(int n)
+{
+	int nums, mul, prodcalcul;
+
+	for (nums = 0; nums <= n; nums++)
+		if (n < 15 && n > 0)
+		{
+			putchar(48);
+			for (mul = 1; mul <= n; mul++)
+			{
+				prodcalcul = nums * mul;
+				putchar(',');
+
+				if (prodcalcul <= 9)
+				{
+					putchar(' ');
+					putchar(' ');
+					putchar(' ');
+					putchar(prodcalcul + 48);
+				}
+				else if (prodcalcul <= 99)
+				{
+					putchar(' ');
+					putchar(' ');
+					putchar((prodcalcul / 10) + 48);
+					putchar((prodcalcul % 10) + 48);
+				}
+
+				else
+				{
+					putchar(' ');
+
+					putchar((prodcalcul / 100) + 48);
+					putchar(((prodcalcul - 100) / 10) + 48);
+					putchar((prodcalcul % 10) + 48);
+				}
+			}
+			putchar('\n');
+		}
+}
