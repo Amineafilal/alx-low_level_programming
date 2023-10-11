1b49544caf1c4cf067058f31e3b1ead4378120db
Amineafilal <amineafilalelalami@gmail.com>
2023-10-11 10:20:46 -0700

    sum ab

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - calculate the sum of 2 integers
+ *
+ * @a: First integer value
+ * @b: Second integer value
+ *
+ * Return: return the value of sum of two int
+*/
+
+int add(int a, int b)
+{
+	int calculadd;
+
+	calculadd = a + b;
+	return (calculadd);
+}
