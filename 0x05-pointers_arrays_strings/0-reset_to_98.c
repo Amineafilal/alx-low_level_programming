2c1d5792823ad13fa274eaebba7a330b193cd988
Amineafilal <amineafilalelalami@gmail.com>
2023-10-16 07:40:44 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - entry point
+ * @n: integer value
+ * Description: Write a function that takes a pointer to an int as parameter
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
