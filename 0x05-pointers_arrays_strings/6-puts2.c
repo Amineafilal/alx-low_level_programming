6e6f5f76bd00ea93d56ba4cbd77200f7f29e8e70
Amineafilal <amineafilalelalami@gmail.com>
2023-10-16 11:38:32 -0700

    6. Half the lies they tell about me arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - entry point
+ * @str: integer value
+ * Description:Write a function that prints every other character of a string,
+ */
+void puts2(char *str)
+{
+	int c, len;
+
+	len = 0;
+	for (len = 0; str[len] != '\0'; len++)
+	{}
+	for (c = 0; c <= len; c = c + 2)
+	{
+		putchar(str[c]);
+	}
+	putchar('\n');
+}
