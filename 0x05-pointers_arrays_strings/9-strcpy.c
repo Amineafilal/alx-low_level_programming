6202e65e0936421b136221843ed45f39e42f2390
Amineafilal <amineafilalelalami@gmail.com>
2023-10-16 15:50:41 -0700

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,18 +8,19 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i, n;
-	char *ret0 = dest;
+	int i;
+	int n = 0;
+	char *add0 = dest;
 
-	n = 0; 
 	while (src[n] != '\0')
 	{
 		n++;
 	}
 	for (i = 0; i <= n; i++)
 	{
-		*dest = src[n];
+		*dest = src[i];
 		dest++;
 	}
-	return (ret0);
+	return (add0);
+}
 }
