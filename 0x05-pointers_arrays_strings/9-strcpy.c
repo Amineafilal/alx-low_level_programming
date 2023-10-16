b75143845932855b1525b4c9806d54ed6f4cbe0f
Amineafilal <amineafilalelalami@gmail.com>
2023-10-16 15:28:38 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcpy -antry point
+ * @dest: First integer value
+ * @src : Second integer value
+ * Description: Write a function that copies the string pointed to by src.
+ * Return: 0
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i, n;
+	char *ret0 = dest + n;
+
+	for (i = 0; src[n] != '\0'; i++)
+	{}
+	for (i = 0; i <= n; i++)
+	{
+		*dest = src[n];
+		*dest++;
+	}
+	return (ret0);
+}
