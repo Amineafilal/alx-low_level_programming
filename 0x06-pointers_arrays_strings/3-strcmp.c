0d9492477d812d5e67ae920ec3a28deead012f2d
Amineafilal <amineafilalelalami@gmail.com>
2023-10-18 12:14:35 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; i < *s1 != '\0' && *s2 != '\0' ; i++)
+	for (i = 0; *s1 != '\0' && *s2 != '\0' ; i++)
 	{
 		if (*s1 != *s2)
 		{
 			return (*s1 - *s2);
 		}
-		*s1++;
-		*s2++;
+		s1++;
+		s2++;
 	}
 	return (*s1 - *s2);
 }
