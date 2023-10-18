3bca3064aff21f77b7579f06a6832109b62e08c2
Amineafilal <amineafilalelalami@gmail.com>
2023-10-18 14:25:43 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *cap_string - Write a function that capitalizes all words of a string.
+ * @o: input
+ * Return: Return the value of o
+*/
+char *cap_string(char *o)
+{
+	int sep = 1;
+
+	for (int i = 0; o[i] != '\0'; i++)
+	{
+		if (sep && ((o[i] >= 97 && o[i] <= 122) || (o[i] >= 65 && o[i] <= 90)))
+		{
+			if (o[i] >= 97 && o[i] <= 122)
+			{
+				o[i] = o[i] - 32;
+			}
+			sep = 0;
+		}
+		else if (o[i] == '.' || o[i] == '-' || o[i] == '\n' || o[i] == '\t')
+		{
+			sep = 1;
+		}
+	}
+	return (o);
+}
