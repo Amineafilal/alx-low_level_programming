512a26334f4c4b22b1631a188c2d542f1c091ab9
Amineafilal <amineafilalelalami@gmail.com>
2023-10-19 08:41:54 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,25 +3,32 @@
  * *cap_string - Write a function that capitalizes all words of a string.
  * @o: input
  * Return: Return the value of o
-*/
+ */
 char *cap_string(char *o)
 {
 	int i;
-	int sep = 1;
+	char caps = 1;
 
 	for (i = 0; o[i] != '\0'; i++)
 	{
-		if (sep && ((o[i] >= 97 && o[i] <= 122) || (o[i] >= 65 && o[i] <= 90)))
+		if (o[i] == '\t')
 		{
-			if (o[i] >= 97 && o[i] <= 122)
-			{
-				o[i] = o[i] - 32;
-			}
-			sep = 0;
+			o[i] = ' ';
 		}
-		else if (o[i] == '.' || o[i] == '-' || o[i] == '\n' || o[i] == '\t')
+		if (caps)
 		{
-			sep = 1;
+			if ((o[i] >= 97 && o[i] <= 122) || (o[i] >= 65 && o[i] <= 90))
+			{
+				if (o[i] >= 97 && o[i] <= 122)
+				{
+					o[i] = o[i] - 32;
+				}
+				caps = 0;
+			}
+		}
+		if (o[i] == ' ' || o[i] == '.' || o[i] == '-')
+		{
+			caps = 1;
 		}
 	}
 	return (o);
