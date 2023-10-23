0bfe0a0ad9a88335828b93f76738893d6a046cf0
Amineafilal <amineafilalelalami@gmail.com>
2023-10-23 06:58:15 -0700

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strstr - Write a function that locates a character in a string.
+ * @haystack: input
+ * @needle: input
+ * Return: Return the value of NULL
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				return (needle + i);
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	return  (NULL);
+}
