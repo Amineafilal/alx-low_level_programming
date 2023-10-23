56d49fa1c0d28fa6e038af46827f82fdd37a889c
Amineafilal <amineafilalelalami@gmail.com>
2023-10-23 06:17:01 -0700

    1. memcpy

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strchr - Write a function that locates a character in a string.
+ * @s: input
+ * @c: input
+ * Return: Return the value of s
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}
