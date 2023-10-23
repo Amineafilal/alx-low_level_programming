a5991485d188042a3fe83b8855605c3368e03451
Amineafilal <amineafilalelalami@gmail.com>
2023-10-23 08:29:46 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,25 +7,21 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	while (*haystack)
+	{
+		char *start = haystack;
+		char *sub = needle;
 
-	if (*needle)
-	{
-		return (haystack);
-	}
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; needle[j] != '\0'; j++)
+		while (*haystack && *sub && *haystack == *sub)
 		{
-			if (haystack[i + j] != needle[j])
-			{
-				return (needle + i);
-			}
+			haystack++;
+			sub++;
 		}
-		if (needle[j] == '\0')
+		if (!*sub)
 		{
-			return (haystack + i);
+			return (start);
 		}
+		haystack = start + 1;
 	}
-	return  (NULL);
+	return (NULL);
 }
