f30ce6786a2400752db5034140e4d1126c747300
Amineafilal <amineafilalelalami@gmail.com>
2023-10-28 07:16:42 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,15 +12,16 @@ int main(int argc, char *argv[])
 {
 	int multip;
 
+	i
 	if (argc != 3)
 	{
 		printf("Error\n");
+		return (1);
 	}
 	else
 	{
 		multip =  atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", multip);
-		return (1);
 	}
 	return (0);
 }
