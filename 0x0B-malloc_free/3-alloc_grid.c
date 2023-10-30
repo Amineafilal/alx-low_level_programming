086aec9530ae618e4c641fc2ab6507173a0cb074
Amineafilal <amineafilalelalami@gmail.com>
2023-10-30 15:02:49 -0700

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * **alloc_grid - Write a function that returns a pointer .
+ * @width: input value
+ * @height: input value
+ * Return: Return value of gridp.
+*/
+int **alloc_grid(int width, int height)
+{
+	int **gridp = 0;
+	int *data = NULL;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	gridp = (int **)malloc(sizeof(int *) * height + sizeof(int) * width * height);
+	if (gridp == NULL)
+	{
+		return (NULL);
+	}
+	data = (int *)(gridp + height);
+	for (i = 0; i < height; i++)
+	{
+		gridp[i] = data + i * width;
+	}
+	for (i = 0; i < height ; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			gridp[i][j] = 0;
+		}
+	}
+	return (gridp);
+}
