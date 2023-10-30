91868aefc0e38ac5813874dee0fd3a6373a9971f
Amineafilal <amineafilalelalami@gmail.com>
2023-10-30 15:40:25 -0700

    4. Its not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,15 +3,12 @@
  * free_grid - Write a function that frees a 2 dimensional grid previously.
  * @grid: input value
  * @height: input value
+ * Return: void
 */
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL || height <= 0)
-	{
-		return;
-	}
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
