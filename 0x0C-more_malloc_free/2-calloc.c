414d861d9364f33397c64aa8231237f4c20823fa
Amineafilal <amineafilalelalami@gmail.com>
2023-11-02 09:21:23 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,43 +4,28 @@
  * @nmemb: input value
  * @size: input value
  * Return: Return value of gridp.
-*/
+ */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int **gridp = 0;
-	unsigned int *data = NULL;
-	unsigned int i, j;
+	char *gridp;
+	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	gridp = (unsigned int **)malloc(sizeof(unsigned int *) * nmemb + sizeof(unsigned int) * nmemb * size);
+	gridp = malloc(nmemb * size);
 	if (gridp == NULL)
 	{
 		return (NULL);
 	}
-	data = (unsigned int *)(gridp + size);
-	for (i = 0; i < size; i++)
+	else
 	{
-		gridp[i] = data + i * nmemb;
-		if (gridp[i] == NULL)
+		for (i = 0; i < (nmemb * size); i++)
 		{
-			for (j = 0; j < i; j++)
-			{
-				free(gridp[i]);
-			}
-			free(gridp);
-			return (NULL);
+			gridp[i] = 0;
 		}
+		return (gridp);
 	}
-	for (i = 0; i < size ; i++)
-	{
-		for (j = 0; j < nmemb; j++)
-		{
-			gridp[i][j] = 0;
-		}
-	}
-	return (gridp);
 }
