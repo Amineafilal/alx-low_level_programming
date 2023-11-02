fcc0772955ca33d9612d9817d8eebb37d66b0193
Amineafilal <amineafilalelalami@gmail.com>
2023-11-02 12:17:13 -0700

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,61 +1,40 @@
 #include  "main.h"
 /**
- * *memcpy - Write a function that copies memory area.
- * @dest: input value
- * @src: input value
- * @n: input value
- * Return: Return the value of dest
-*/
-void *memcpy(void *dest, const void *src, unsigned int n)
-{
-	unsigned int i;
-	char *d = (char *)dest;
-	const char *s = (const char *)src;
-
-	for (i = 0; i < n; i++)
-	{
-		d[i] = s[i];
-	}
-	return (dest);
-}
-/**
  * *_realloc - function that reallocates a memory block
  * @ptr: input value
  * @old_size: size of ptr
  * @new_size: size of the newly allocated block of memory.
  * Return: Return value of p
-*/
+ */
 
-void *_realloc(void *ptr, size_t old_size, size_t new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+	void *new_ptr = malloc(new_size);
+	char *p = ptr;
+	char *new_p = new_ptr;
+	unsigned int i;
 
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
+	if (new_ptr == NULL)
 	{
-		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-		{
-			return (NULL);
-		}
+		return (NULL);
 	}
-	else if (new_size <= old_size)
+	for (i = 0; i < old_size && i < new_size; i++)
 	{
-		new_ptr = ptr;
+		new_p[i] = p[i];
 	}
-	else
-	{
-		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-		{
-			return (NULL);
-		}
-		memcpy(new_ptr, ptr, old_size);
-		free(ptr);
-	}
+	free(ptr);
 	return (new_ptr);
 }
