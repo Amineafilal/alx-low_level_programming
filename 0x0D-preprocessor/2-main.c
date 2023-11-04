b23f96d7941f7872fa2ac3e67cb8c6c44493e16b
Amineafilal <amineafilalelalami@gmail.com>
2023-11-04 10:08:22 -0700

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the name of the file
+ * Return: Return 0 (succes)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
