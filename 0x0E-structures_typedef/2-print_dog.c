cfaed08a6bba94557b6b64b615b3a2e7f1c5037a
Amineafilal <amineafilalelalami@gmail.com>
2023-11-04 16:27:19 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if (d->name == NULL)
+	if ((*d).name == NULL)
 	{
 		printf("nil\n");
 	}
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name : %s\n", d->name);
 	}
-	if (d->age < 0)
+	if ((*d).age < 0)
 	{
 		printf("nil\n");
 	}
@@ -25,7 +25,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Age : %f\n", d->age);
 	}
-	if (d->owner == NULL)
+	if ((*d).owner == NULL)
 	{
 		printf("nil\n");
 	}
