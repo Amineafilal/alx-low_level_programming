9c4c3c7da1427d63a7c6343b261419914bcc734a
Amineafilal <amineafilalelalami@gmail.com>
2023-11-04 16:18:50 -0700

    task 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,19 +10,13 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner's name
-*/
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-
-/**
- * dog_t - Typedef for struct dog
-*/
-typedef struct dog dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
