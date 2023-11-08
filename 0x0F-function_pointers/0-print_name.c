c9c1f579dd8ecb9054c30586f1ce74ff30b04fa3
Amineafilal <amineafilalelalami@gmail.com>
2023-11-08 07:15:52 -0800

    0. Whats my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - Write a function that prints a name.
+ * @name: input value
+ * @f: function to pointer
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
