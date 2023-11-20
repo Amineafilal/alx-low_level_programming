62d1f6fa18d8f2eb50b6594d5e2f9836f51951f5
Amineafilal <amineafilalelalami@gmail.com>
2023-11-20 12:01:06 -0800

    10. Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		nodeindex = nodeindex->next;
 	}
-	if (nodeindex == NULL ||  nodeindex->next)
+	if (nodeindex == NULL ||  nodeindex->next == NULL)
 	{
 		return (-1);
 	}
