e2726b40fde49b8eed21360b046613052540b906
Amineafilal <amineafilalelalami@gmail.com>
2023-11-21 14:03:01 -0800

    fixed task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,17 +8,20 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *current = head;
-	while (current != NULL)
+	long int diff;
+
+	while (head)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
+		diff = head - head->next;
 		count++;
-		if (current <= current->next)
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
 		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			exit(98);
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
 		}
-		current = current->next;
 	}
 	return (count);
 }
