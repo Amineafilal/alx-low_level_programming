714afef9658085560aa00701fecd75bdfc04d144
Amineafilal <amineafilalelalami@gmail.com>
2023-11-27 11:23:04 -0800

    Fixed errors task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,27 +7,36 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t r, w, o;
+	int fd;
+	ssize_t r, w;
 	char *buffer;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-	buffer = malloc(letters);
-	if (buffer == NULL)
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
 	{
 		return (0);
 	}
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
 		free(buffer);
 		return (0);
 	}
+	r = read(fd, buffer, letters);
+	if (r == -1)
+	{
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1 || r != w)
+	{
+		return (0);
+	}
 	free(buffer);
-	close(o);
+	close(fd);
 	return (w);
 }
