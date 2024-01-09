dfe30914d14edbc56f4e3ea1481ad6e1ebb0bda6
Amineafilal <amineafilalelalami@gmail.com>
2024-01-08 23:13:04 -0800

    fixed error

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,6 +7,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count_node = 0;
+
 	if (h == NULL)
 	{
 		return (count_node);
@@ -14,12 +15,12 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h->prev != NULL)
 	{
 		h = h->prev;
-	}	
+	}
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count_node++;
-	}	
+	}
 	return (count_node);
 }
