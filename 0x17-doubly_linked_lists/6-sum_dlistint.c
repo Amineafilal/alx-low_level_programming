5b2c02f81560d92d753a5a9b81e8a898071c96af
Amineafilal <amineafilalelalami@gmail.com>
2024-01-10 00:06:54 -0800

    fixed error

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_dlistint -  function that returns the sum of all the data.
+ * @head: pointer to pointer to head of list
+ * Return: return value of sum
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
